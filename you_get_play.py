42d2b6fd70c31d4e2328d240189f685e38ed571a
West Yao <weihuayao@gmail.com>
2016-01-22 18:42:12 +0800

    add history menu

--- you_get_play.py
+++ you_get_play.py
@@ -1,11 +1,12 @@
 import sys
 import subprocess
 import urllib.request
-
+import pickle
 from PyQt4 import QtGui
 
 
 class YouGetPlay(QtGui.QWidget):
+    __HISTORY_FILENAME__ = '.player_history'
 
     def __init__(self, parent):
         super(YouGetPlay, self).__init__(parent)
@@ -21,17 +22,55 @@ class YouGetPlay(QtGui.QWidget):
             "&Enable", self, shortcut='Ctrl+E',
             statusTip='Enable monitor the clipboard', checkable=True)
 
+        self.historyMenu = QtGui.QMenu("&History")
+
         self.enableAction.setChecked(True)
 
+        self.menu.addMenu(self.historyMenu)
         self.menu.addAction(self.enableAction)
         self.menu.addAction(exitAction)
 
         self.tray.setContextMenu(self.menu)
         self.tray.setToolTip('you-get play')
+        self.history = []
+        self.load_history()
+
+    def close(self):
+        self.save_history()
+        super().close()
+
+    def load_history(self):
+        try:
+            with open(self.__HISTORY_FILENAME__, 'rb') as f:
+                self.history = pickle.load(f)
+        except:
+            pass
+        self.update_history_menu()
+
+    def save_history(self):
+        with open(self.__HISTORY_FILENAME__, 'wb') as f:
+            pickle.dump(self.history, f, -1)
+
+    def append_history(self, url):
+        if len(self.history) > 10:
+            self.history = self.history[1:]
+        self.history.append(url)
+        self.update_history_menu()
+
+    def update_history_menu(self):
+        self.historyMenu.clear()
+        last = None
+        for url in self.history:
+            action = QtGui.QAction(url, self, triggered=lambda: self.play(url))
+            self.historyMenu.insertAction(last, action)
+            last = action
 
     def play(self, url):
         p = subprocess.Popen(['you-get', '-p', 'mpv', url])
         p.wait()
+        if url in self.history:
+            self.history.remove(url)
+        self.append_history(url)
 
     def onClipChanged(self):
         if(QtGui.QApplication.clipboard().mimeData().hasText()):
@@ -42,19 +81,15 @@ class YouGetPlay(QtGui.QWidget):
                     urllib.request.urlopen(text)
                     print('Playing ... ')
                     if QtGui.QSystemTrayIcon.supportsMessages():
-                        self.tray.showMessage('Now Playing ...',  text)
+                        self.tray.showMessage('Now Playing ...', text)
                     self.play(text)
                     print('End')
                 except Exception as e:
                     print(e)
 
-
-def main():
+if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
     frame = YouGetPlay(None)
     frame.tray.show()
     app.clipboard().dataChanged.connect(frame.onClipChanged)
     sys.exit(app.exec_())
-
-if __name__ == '__main__':
-    main()
